63cc06913ffd30f6280949be5b340053dea00248
Student <student@ib.polsl.pl>
2017-02-27 12:48:05 +0100

    Piaty commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "LaborkaConfig.h"
 #ifdef USE_TRIGONOMETRY_DEGREE
 #include "Trygonometria.h"
@@ -10,9 +11,10 @@ int main()
 {
 double result;
 #ifdef USE_TRIGONOMETRY_DEGREE
-	result = cos_degree(45.0);
+	result = sin_degree(45.0);
 #else
-	result = cos(M_PI/4.0);
+	result = sin(M_PI/4.0);
 #endif
+	std::cout << result << std::endl;
 return 0;
 }
